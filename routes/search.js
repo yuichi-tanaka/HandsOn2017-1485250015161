de7f82cb1bd7eb6a99815cfa3717532f67bfd4c1
yuichi-tanaka <tanaka.y.p@gmail.com>
2017-01-24 19:44:45 +0900

    search initial commit

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var _S = require("../src/search");
+var search = new _S;
+console.dir(search.getSearch());
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', searchData: search.getSearch() });
 });
 
 module.exports = router;
---
+++ src/search.js
@@ -0,0 +1,11 @@
+var search = module.exports = function(){};
+search.prototype.getSearch = function(){
+  //todo:
+  //
+  return [
+    "追いかける恋がいい","追われる恋がいい",
+    "面白くて飽きない人","一緒にいると癒される",
+    "ルックス重視","人柄重視",
+    "プロポーズは派手なサプライズ","プロポーズは地味なレストラン"
+  ];
+};
